5870b1721d08020b3c3e8e48fa3ead98ea011d61
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2024-10-15 22:37:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -34,7 +34,7 @@ STREAM_API = (environ.get('STREAM_API', 'zu1xmKAMdlguQSBt5c8ais2aV212'))
 STREAMHTO = (environ.get('STREAMHTO', 'https://t.me/+mAdUKj6Jc40yYWVl'))
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5597521952 6789146594').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5597521952 6789146594').split( )]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '').split()] #Channel id for auto indexing ( make sure bot is admin )
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
