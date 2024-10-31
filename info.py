4e7d1f02953265f084b17e0501c30f6e20afcbcc
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2024-10-31 13:23:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -105,7 +105,7 @@ if 'DYNO' in environ:
 else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
-FQDN = str(getenv('FQDN', '13.61.7.238')) #if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
+FQDN = str(getenv('FQDN', '166.0.242.161')) #if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
 
 
 URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
