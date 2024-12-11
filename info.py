eff291a4b5a7a51d5c46f363caee8da888baf590
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2024-12-12 05:29:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -52,7 +52,7 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False)) # True if you want n
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Rename_1by1_robot:bve7Z3HNBQEbDM3t@cluster0.r7y7j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "futurepunk")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'futurepunk')
 
 # Verify
 VERIFY = bool(environ.get('VERIFY', False)) # Verification On ( True ) / Off ( False )
